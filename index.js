5dff3498f09207d2d396fe5e1c4b9c64295eb4c3
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-08 20:04:39 +0530

    Infinite-scrolling with js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const url =
 let limit = 5;
 let count = 1;
 let posts = 1;
-async function getImages() {
+async function getPosts() {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${count}`
   );
@@ -24,4 +24,25 @@ async function getImages() {
     container.insertAdjacentHTML("beforeend", data);
   });
 }
-getImages();
+getPosts();
+
+const showMore=() =>
+{
+    setTimeout(() =>
+    {
+        count++;
+        getPosts()
+    },300)
+}
+window.addEventListener('scroll',() =>
+{
+    //scrollhight=total contetnt length
+    //clientheight= data we can watch on screen present
+    const {scrollHeight,scrollTop,clientHeight}=document.documentElement;
+
+    if(scrollTop+clientHeight>=scrollHeight)
+    {
+        console.log("we are on end or bottom of scroll")
+        showMore()
+    }
+})
