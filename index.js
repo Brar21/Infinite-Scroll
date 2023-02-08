26e3ffd7110af2db67a99b34c435c61f852fb670
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-08 17:52:38 +0530

    Infinite-scrolling with js

--- index.js
+++ index.js
@@ -9,18 +9,22 @@ async function getImages()
     const res=await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${count}`)
     const data=await res.json()
     console.log(data)
-    data.forEach((el,index) =>
+    data.map((el,index) =>
     {
         let container=document.querySelector('.container')
+        const data=`
+        <p class='number'>${el.id}</p>
+    <h1 class='title'>${el.title}</h1>
+        `
         let div=document.createElement('div')
-        let number=document.createElement('p')
-        number.classList.add('number')
-        number=el.id
-        let title=document.createElement('h1')
-        title.classList.add('title')
-        title=el.title
-        div.append(number,title)
-        container.append(div)
+        //let number=document.createElement('p')
+        //number.classList.add('number')
+        //number=el.id
+        //let title=document.createElement('h1')
+        //title.classList.add('title')
+        //title=el.title
+        container.insertAdjacentHTML('beforeend',data)
+        //container.append(div)
     })
 }
 getImages()
