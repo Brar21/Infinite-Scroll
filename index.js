a2329f61926e849edf18202ce6e8ba0ef23b425a
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-08 18:01:20 +0530

    Infinite-scrolling with js

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const container=document.querySelector('.container')
-const url='https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${count}'
+const container = document.querySelector(".container");
+const url =
+  "https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${count}";
 
-let limit=5;
-let count=1;
-let posts=1
-async function getImages()
-{
-    const res=await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${count}`)
-    const data=await res.json()
-    console.log(data)
-    data.map((el,index) =>
-    {
-        let container=document.querySelector('.container')
-        const data=`
+let limit = 5;
+let count = 1;
+let posts = 1;
+async function getImages() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${count}`
+  );
+  const data = await res.json();
+  console.log(data);
+  data.map((el, index) => {
+    let container = document.querySelector(".container");
+    const data = `
+        <div>
         <p class='number'>${el.id}</p>
-    <h1 class='title'>${el.title}</h1>
-        `
-        let div=document.createElement('div')
-        //let number=document.createElement('p')
-        //number.classList.add('number')
-        //number=el.id
-        //let title=document.createElement('h1')
-        //title.classList.add('title')
-        //title=el.title
-        container.insertAdjacentHTML('beforeend',data)
-        //container.append(div)
-    })
+        <h1 class='title'>${el.title}</h1>
+        <p class='body'>${el.body}</p>
+         </div>
+        `;
+
+    container.insertAdjacentHTML("beforeend", data);
+  });
 }
-getImages()
+getImages();
