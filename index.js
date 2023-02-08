e32623767fe62ea5ea9bd927e93be090e3799a69
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-02-08 17:40:05 +0530

    Infinite-scrolling with js

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const container=document.querySelector('.container')
-const url='http://api.adorable.io/avatars/'
+const url='https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${count}'
 
-function getImages(n=10)
+let limit=5;
+let count=1;
+let posts=1
+async function getImages()
 {
-    let i=0;
-    while(i<n)
+    const res=await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${count}`)
+    const data=await res.json()
+    console.log(data)
+    data.forEach((el,index) =>
     {
-        const img=document.createElement('img')
-        img.src=`${url}${getRandom()}`
-        container.appendChild(img)
-        i++
-    }
+        let container=document.querySelector('.container')
+        let number=document.createElement('p')
+        number=el.id
+        let title=document.createElement('h1')
+        title=el.title
+        container.append(number,title)
+    })
 }
 getImages()
-function getRandom()
-{
-    Math.floor(Math.random()*100)
-}
